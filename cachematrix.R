d5e801eceeab0b8f20369e24f841139e6b0c58d8
cloudarch <gpadakandla@gmail.com>
2014-11-22 23:16:21 -0800

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix 
+## Two helper functions to cache inverse of matrix to save repeated long running computations 
+## for the same result
 
-## Write a short comment describing this function
+## makeCacheMatrix : 
+## caches given matrix and its inverse into global space and
+## returns a list of constructors to get or set global variables above
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  s <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  
+  get <- function() x
+  
+  setsolve <- function(solve) s <<- solve
+  
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
+## cacheSolve : 
+## Return inverse of given CacheMatrix if in cache else first calculate, cache and finally return
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = makeCacheMatrix, ...) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
